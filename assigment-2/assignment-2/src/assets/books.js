da3be1f3eb880329cc5b1beb17f4705e26dcb60d
abdurrahman050 <abdurrahmanmunir5149@gmail.com>
2024-11-28 23:12:49 +0500

    Add files via upload

---
+++ assigment-2/assignment-2/src/assets/books.js
@@ -0,0 +1,13 @@
+
+
+const Books = [
+    { id: 1, title: "The Catcher in the Rye", author: "J.D. Salinger", rating: 4.5, genre: "Fiction" },
+    { id: 2, title: "To Kill a Mockingbird", author: "Harper Lee", rating: 4.8, genre: "Fiction" },
+    { id: 3, title: "1984", author: "George Orwell", rating: 4.7, genre: "Fiction" },
+    { id: 4, title: "Brave New World", author: "Aldous Huxley", rating: 4.6, genre: "Science" },
+    { id: 5, title: "The Great Gatsby", author: "F. Scott Fitzgerald", rating: 4.3, genre: "Fiction" },
+   
+  ];
+  
+  export default Books;
+  
